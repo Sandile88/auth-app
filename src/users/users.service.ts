d36ca8e00916613b6dabf2270127f7da4a783b06
Sandile88 <sandilep.mremi@gmail.com>
2025-12-28 21:18:14 +0200

    added more funcs in svc file

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
-import { User, UserSchema } from './schemas/user.schema';
+import { User } from './schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -15,10 +15,32 @@ export class UsersService {
             
         // }
 
-        const userAlreadyExists = await this.userModel.findOne( createUserDto.email);
+        const userAlreadyExists = await this.userModel.findOne({ email: createUserDto.email});
         if (userAlreadyExists) {
             throw new ConflictException("User already exists!");
         }
+
+        const user = new this.userModel({
+            ...createUserDto
+        })
+
+        return user.save();
+    }
+
+    async findByEmail( email: string) {
+        return this.userModel.findOne({ email });
+    }
+
+    async findById(userId: string) {
+        return this.userModel.findById(userId);
+    }
+
+    async updateUserDetails(userId: string, updates: Partial<CreateUserDTO>) {
+        return this.userModel.findByIdAndUpdate(userId, updates, { new: true });
+    }
+
+    async deleteUser(userId: string) {
+        return this.userModel.findByIdAndDelete(userId);
     }
 
 }
