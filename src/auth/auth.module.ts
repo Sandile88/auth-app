eccc2db887859f60cd8d17bdc308225c8c25c1c9
Sandile88 <sandilep.mremi@gmail.com>
2025-12-31 01:02:13 +0200

    added guard, custom decorator for cleaner code, remove endpoints in users.controller.ts, implemented passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,31 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { EmailService } from 'src/email/email.service';
+import { EmailModule } from 'src/email/email.module';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-    imports: [UsersModule, JwtModule.register({ 
-        secret: 'JWT_SECRET', signOptions: {expiresIn: '1d'}}), EmailService],
-    providers: [AuthService],
+    imports: [
+        UsersModule, 
+        EmailModule, 
+        PassportModule ,
+        JwtModule.registerAsync({ 
+        useFactory: async (configService: ConfigService) => ({
+            secret: configService.get<string>('JWT_SECRET'), 
+            signOptions: {
+                expiresIn: '1d',
+                },
+        }),
+        inject: [ConfigService],
+    }),
+    ],
+    providers: [AuthService, JwtStrategy],
     controllers: [AuthController],
+    exports: [AuthService],
+
 })
-export class AuthModule {
-}
+
+export class AuthModule {}
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,50 +1,26 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdateUserDTO } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/shared/decorators/current-user.decorator';
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
-    // @Get('email/:email')
-    // getByEmail(@Param('email') email: string) {
-    //     return this.usersService.findByEmail(email);
-    // }
-
-    // @Get(':id')
-    // getById(@Param('id') id: string) {
-    //     return this.usersService.findById(id);
-    // }
-
-    @Post()
-    create(@Body() createUserDto: CreateUserDTO) {
-        return this.usersService.createUser(createUserDto);
+    @Get('me')
+    @UseGuards(JwtAuthGuard)
+    getMe(@CurrentUser() user) {
+        return this.usersService.findById(user.userId);
     }
 
-    @Patch(':id')
-    updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDTO) {
-        return this.usersService.updateUserDetails(id, updateUserDto);
+    
+    @Patch('me')
+    @UseGuards(JwtAuthGuard)
+    updateUser(@CurrentUser() user, @Body() updateUserDto: UpdateUserDTO) {
+        return this.usersService.updateUserDetails(user.userId, updateUserDto);
     }
 
-    // @Delete(':id')
-    // removeUser(@Param('id') id: string) {
-    //     return this.usersService.deleteUser(id);
-    // }
-
-    // @Patch(':id/verify')
-    // updateVerificationStatus(@Param('id') id: string) {
-    //     return this.usersService.markUserAsVerified(id);
-    // }
-
-    @Patch(':id/login')
-    updateLastLoginDate(@Param('id') id: string) {
-        return this.usersService.updateLastLogin(id);
-    }
-
-    // @Patch(':id/deactivate')
-    // deactiveUserStatus(@Param('id') id: string) {
-    //     return this.usersService.deactivateUser(id);
-    // }
 
 }
